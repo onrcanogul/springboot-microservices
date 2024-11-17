096aa190d2d76208cec7ab4c54f09a891500d73e
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2024-11-17 23:27:53 +0300

    change configs options

--- accounts/src/main/java/com/bankapp/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/com/bankapp/accounts/dto/AccountsContactInfoDto.java
@@ -1,12 +1,17 @@
 package com.bankapp.accounts.dto;
 
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Map;
 import java.util.List;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport){
-
+@Getter @Setter
+public class AccountsContactInfoDto{
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
--- cards/src/main/java/com/bankapp/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/bankapp/cards/dto/CardsContactInfoDto.java
@@ -1,5 +1,7 @@
 package com.bankapp.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -7,5 +9,9 @@ import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto (String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class CardsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
